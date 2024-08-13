854e2477524a351718c795ec333e16668c53f218
riku546 <opa9788@gmail.com>
2024-08-14 00:52:57 +0900

    strategyパターン

--- design/strategy.php
+++ design/strategy.php
@@ -1,45 +1,45 @@
 <?php
 
-interface Strategy
-{
-    public function processPay(int $amount);
-}
+// interface Strategy
+// {
+//     public function processPay(int $amount);
+// }
 
-class CreditCard implements Strategy
-{
-    public function processPay(int $amount)
-    {
-        echo htmlspecialchars("success creditCard payment ");
-    }
-}
+// class CreditCard implements Strategy
+// {
+//     public function processPay(int $amount)
+//     {
+//         echo htmlspecialchars("success creditCard payment ");
+//     }
+// }
 
-class PayPay implements Strategy
-{
-    public function processPay(int $amount)
-    {
-        echo htmlspecialchars("success paypay payment");
-    }
+// class PayPay implements Strategy
+// {
+//     public function processPay(int $amount)
+//     {
+//         echo htmlspecialchars("success paypay payment");
+//     }
 
-}
+// }
 
-class Payment
-{
-    private $paymentStrategy;
+// class Payment
+// {
+//     private $paymentStrategy;
 
-    public function __construct(Strategy $strategy)
-    {
-        $this->paymentStrategy = $strategy;
+//     public function __construct(Strategy $strategy)
+//     {
+//         $this->paymentStrategy = $strategy;
 
-    }
+//     }
 
-    public function executePayment($amount)
-    {
-        $this->paymentStrategy->processPay($amount);
-    }
+//     public function executePayment($amount)
+//     {
+//         $this->paymentStrategy->processPay($amount);
+//     }
 
-}
+// }
 
-// $credit = new CreditCard();
-$paypay = new PayPay();
-$payment = new Payment($paypay);
-$payment->executePayment(100);
+// // $credit = new CreditCard();
+// $paypay = new PayPay();
+// $payment = new Payment($paypay);
+// $payment->executePayment(100);
